62e469a55795cca80e43cd5f54ef2c1b34ac3f04
Ertinox45 <68199251+Ertinox45@users.noreply.github.com>
2023-12-30 17:48:47 +0100

    Add Anti Puncture

---
+++ src/main/java/fr/dynamx/addons/immersive/common/infos/AntiPunctureInfo.java
@@ -0,0 +1,39 @@
+package fr.dynamx.addons.immersive.common.infos;
+
+import fr.dynamx.api.contentpack.object.subinfo.ISubInfoTypeOwner;
+import fr.dynamx.api.contentpack.object.subinfo.SubInfoType;
+import fr.dynamx.api.contentpack.registry.DefinitionType;
+import fr.dynamx.api.contentpack.registry.PackFileProperty;
+import fr.dynamx.api.contentpack.registry.RegisteredSubInfoType;
+import fr.dynamx.api.contentpack.registry.SubInfoTypeRegistries;
+
+@RegisteredSubInfoType(
+        name = "HerseAddon",
+        registries = {SubInfoTypeRegistries.WHEELS},
+        strictName = false
+)
+public class AntiPunctureInfo<T extends ISubInfoTypeOwner<T>> extends SubInfoType<T> {
+
+    @PackFileProperty(configNames = {"CanFlattened"}, type = DefinitionType.DynamXDefinitionTypes.BOOL)
+    public boolean canFlattened;
+
+    public AntiPunctureInfo(ISubInfoTypeOwner<T> owner) {
+        super(owner);
+    }
+
+    @Override
+    public void appendTo(T partWheelInfo) {
+        owner.addSubProperty(this);
+    }
+
+    @Override
+    public String getName() {
+        return "AntiPunctureInfo of " + getOwner().getFullName();
+    }
+
+    @Override
+    public String getPackName() {
+        return owner.getPackName();
+    }
+
+}
